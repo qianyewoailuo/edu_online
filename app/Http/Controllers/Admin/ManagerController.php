6423ed5d79ad37e7292f27b82f31c444552c0f15
qianyewoailuo <qianyewoailuo@gmail.com>
2019-03-13 19:47:48 +0800

    admin.manager.index

---
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Admin\Manager;
+
+class ManagerController extends Controller
+{
+    //管理员首页列表
+    public function index()
+    {
+        $data = Manager::get();
+        // laravel传递模板数据的方法是view(模板路由,数据)
+        return view('admin/manager/index',compact('data'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,12 @@ Route::group(['prefix' => 'admin'],function(){
     // 登录验证页面
     Route::post('public/check','Admin\PublicController@check')->name('check');
 });
-// 后台路由组-权限
-Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
+// 后台路由组-权限 
+// 使用中间件middleware其参数必须指定自定义的guard,即auth:guard name
+Route::group(['prefix'=>'admin','middleware'=>'auth:admin'],function(){
     // 后台首页路由
     Route::get('index/index','Admin\IndexController@index');
     Route::get('index/welcome','Admin\IndexController@welcome');
-    // test 路由
-    Route::get('index/test','Admin\IndexController@test');
+    // 管理员列表首页路由
+    Route::get('manager/index','Admin\ManagerController@index');
 });
